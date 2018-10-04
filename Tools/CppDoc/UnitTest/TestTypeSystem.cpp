23bfa8133e5261f0144cb37110bfec7009bbf0c2
Zihan Chen <vczh@163.com>
2018-10-03 21:28:12 -0700

    ...

--- Tools/CppDoc/UnitTest/TestTypeSystem.cpp
+++ Tools/CppDoc/UnitTest/TestTypeSystem.cpp
@@ -2,6 +2,25 @@
 
 TEST_CASE(TestTypeSystem_Primitive)
 {
+	auto tsys = ITsysAlloc::Create();
+#define TEST_PRIMITIVE_BYTES(TYPE, BYTES) TEST_ASSERT(tsys->PrimitiveOf({TsysPrimitiveType::TYPE,TsysBytes::BYTES})==tsys->PrimitiveOf({TsysPrimitiveType::TYPE,TsysBytes::BYTES}))
+#define TEST_PRIMITIVE(TYPE)\
+	TEST_PRIMITIVE_BYTES(TYPE, _1);\
+	TEST_PRIMITIVE_BYTES(TYPE, _2);\
+	TEST_PRIMITIVE_BYTES(TYPE, _4);\
+	TEST_PRIMITIVE_BYTES(TYPE, _8);\
+	TEST_PRIMITIVE_BYTES(TYPE, _10);\
+	TEST_PRIMITIVE_BYTES(TYPE, _16)
+
+	TEST_PRIMITIVE(Signed);
+	TEST_PRIMITIVE(Unsigned);
+	TEST_PRIMITIVE(Float);
+	TEST_PRIMITIVE(Char);
+	TEST_PRIMITIVE(Bool);
+	TEST_PRIMITIVE(Void);
+
+#undef TEST_PRIMITIVE
+#undef TEST_PRIMITIVE_BYTES
 }
 
 TEST_CASE(TestTypeSystem_Decl)
