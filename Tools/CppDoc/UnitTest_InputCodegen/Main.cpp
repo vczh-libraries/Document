4320465b942bbc278c12318564e736305105ff05
Zihan Chen <vczh@163.com>
2019-03-03 02:14:52 -0800

    ...

--- Tools/CppDoc/UnitTest_InputCodegen/Main.cpp
+++ Tools/CppDoc/UnitTest_InputCodegen/Main.cpp
@@ -11,10 +11,11 @@ namespace operator_overloading
 		const wchar_t* name;
 		bool parameter;
 
+		Op() = default;
 		Op(const wchar_t* _name, bool _parameter) :name(_name), parameter(_parameter) {}
 	};
 
-	Op ops[] = {
+	Op ops1[] = {
 		{L"++",		true},
 		{L"--",		true},
 		{L"++",		false},
@@ -25,6 +26,9 @@ namespace operator_overloading
 		{L"+",		false},
 		{L"&",		false},
 		{L"*",		false},
+	};
+
+	Op ops2[] = {
 		{L"*",		true},
 		{L"/",		true},
 		{L"%",		true},
@@ -57,6 +61,8 @@ namespace operator_overloading
 		{L",",		true},
 	};
 
+	Op ops[(sizeof(ops1) + sizeof(ops2)) / sizeof(Op)];
+
 	void GenerateInput1()
 	{
 		FilePath path = L"../UnitTest/TestOverloadingOperator_Input1.h";
@@ -84,12 +90,57 @@ namespace operator_overloading
 			writer.WriteString(op.name[1] ? L"\t" : L"\t\t");
 			writer.WriteLine(op.parameter ? L"(int)const;" : L"()const;");
 		}
-		writer.WriteLine(L"\t}");
+		writer.WriteLine(L"\t};");
 		writer.WriteLine(L"}");
 	}
 
 	void GenerateInput2()
 	{
+		FilePath path = L"../UnitTest/TestOverloadingOperator_Input2.h";
+		FileStream fileStream(path.GetFullPath(), FileStream::WriteOnly);
+		Utf8Encoder encoder;
+		EncoderStream encoderStream(fileStream, encoder);
+		StreamWriter writer(encoderStream);
+
+		writer.WriteLine(L"namespace test_overloading");
+		writer.WriteLine(L"{");
+		writer.WriteLine(L"\tstruct Y");
+		writer.WriteLine(L"\t{");
+		writer.WriteLine(L"\t};");
+		for (auto op : ops)
+		{
+			if (wcscmp(op.name, L"=") == 0) continue;
+			writer.WriteString(L"\t\tvoid* operator");
+			writer.WriteString(op.name);
+			writer.WriteString(op.name[1] ? L"\t\t" : L"\t\t\t");
+			writer.WriteLine(op.parameter ? L"(Y&, int);" : L"(Y&);");
+		}
+		for (auto op : ops2)
+		{
+			if (wcscmp(op.name, L"=") == 0) continue;
+			writer.WriteString(L"\t\tvoid* operator");
+			writer.WriteString(op.name);
+			writer.WriteString(op.name[1] ? L"\t\t" : L"\t\t\t");
+			writer.WriteLine(L"(int, Y&);");
+		}
+		writer.WriteLine(L"");
+		for (auto op : ops)
+		{
+			if (wcscmp(op.name, L"=") == 0) continue;
+			writer.WriteString(L"\t\tvoid* operator");
+			writer.WriteString(op.name);
+			writer.WriteString(op.name[1] ? L"\t\t" : L"\t\t\t");
+			writer.WriteLine(op.parameter ? L"(const Y&, int);" : L"(const Y&);");
+		}
+		for (auto op : ops2)
+		{
+			if (wcscmp(op.name, L"=") == 0) continue;
+			writer.WriteString(L"\t\tvoid* operator");
+			writer.WriteString(op.name);
+			writer.WriteString(op.name[1] ? L"\t\t" : L"\t\t\t");
+			writer.WriteLine(L"(int, const Y&);");
+		}
+		writer.WriteLine(L"}");
 	}
 
 	void GenerateInput3()
@@ -99,8 +150,14 @@ namespace operator_overloading
 
 int main()
 {
-	operator_overloading::GenerateInput1();
-	operator_overloading::GenerateInput2();
-	operator_overloading::GenerateInput3();
+	{
+		using namespace operator_overloading;
+
+		memcpy(&ops[0], &ops1[0], sizeof(ops1));
+		memcpy(&ops[sizeof(ops1) / sizeof(Op)], &ops2[0], sizeof(ops2));
+		GenerateInput1();
+		GenerateInput2();
+		GenerateInput3();
+	}
 	return 0;
 }
