8741fb1fbbfeb8f71db46103a7d8075aa066c002
Zihan Chen <vczh@163.com>
2019-03-04 04:59:45 -0800

    Finished TestGF_Features

--- Tools/CppDoc/Core/Source/Ast_Type_GenerateMembers.cpp
+++ Tools/CppDoc/Core/Source/Ast_Type_GenerateMembers.cpp
@@ -290,10 +290,7 @@ Ptr<ForwardFunctionDeclaration> GenerateAssignOp(Symbol* classSymbol, bool delet
 		decl->type = funcType;
 		
 		funcType->returnType = GenerateRefType(classSymbol, CppReferenceType::LRef);
-		if (parameter)
-		{
-			funcType->parameters.Add(parameter);
-		}
+		funcType->parameters.Add(parameter);
 	}
 	if (!deleted) decl->decoratorDefault = true;
 	if (deleted) decl->decoratorDelete = true;
