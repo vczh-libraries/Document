d020c616bb9aa2b8e9577b9b8a352f828106f32b
Zihan Chen <vczh@163.com>
2019-03-11 18:41:30 -0700

    ...

--- Tools/CppDoc/UnitTest_Cases/Cpp.js
+++ Tools/CppDoc/UnitTest_Cases/Cpp.js
@@ -13,10 +13,46 @@ function turnOnSymbol(id) {
     }
 }
 
+function jumpToSymbolInThisPage(id) {
+    turnOnSymbol(id);
+    if (lastFocusedElement !== undefined) {
+        window.location.href = '#';
+        window.location.href = '#' + symbolId;
+    }
+}
+
+function jumpToSymbolInOtherPage(id, file) {
+    alert('Not implemented.');
+}
+
 function jumpToSymbol(overloadResolutions, resolved) {
     for (ids of [overloadResolutions, resolved]) {
         for (id of ids) {
-
+            let symbol = referencedSymbols[id];
+            if (symbol !== undefined) {
+                if (symbol.definition === true) {
+                    let symbolId = 'Decl$' + id;
+                    let file = symbolToFiles[symbolId];
+                    if (file === '') {
+                        jumpToSymbolInThisPage(symbolId);
+                        return;
+                    } else if (file !== undefined) {
+                        jumpToSymbolInOtherPage(symbolId, file);
+                        return;
+                    }
+                }
+                for (i = 0; i < symbol.declarations; i++) {
+                    let symbolId = 'Forward[' + i + ']$' + id;
+                    let file = symbolToFiles[symbolId];
+                    if (file === '') {
+                        jumpToSymbolInThisPage(symbolId);
+                        return;
+                    } else if (file !== undefined) {
+                        jumpToSymbolInOtherPage(symbolId, file);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
