db1c7f2c1c0ce664d7415c3d861505cc07ca5697
Zihan Chen <vczh@163.com>
2019-04-28 01:39:58 -0700

    ...

--- Tools/CppDoc/UnitTest/TestParseGenericTypeAlias.cpp
+++ Tools/CppDoc/UnitTest/TestParseGenericTypeAlias.cpp
@@ -167,6 +167,10 @@ using Func = R(*)(TArgs...);
 )";
 	COMPILE_PROGRAM(program, pa, input);
 	
+	AssertType(pa, L"SizeOfTypes",									L"SizeOfTypes",									L"<::SizeOfTypes::[...TTypes]> unsigned __int32"			);
+	AssertType(pa, L"SizeOfExprs",									L"SizeOfExprs",									L"<::SizeOfExprs::[...TTypes]> unsigned __int32"			);
+	AssertType(pa, L"Func",											L"Func",										L"<::Func::[R], ::Func::[...TArgs]> any_t"					);
+	
 	AssertType(pa, L"SizeOfTypes<>",								L"SizeOfTypes<>",								L"unsigned __int32"											);
 	AssertType(pa, L"SizeOfTypes<int>",								L"SizeOfTypes<int>",							L"unsigned __int32"											);
 	AssertType(pa, L"SizeOfTypes<int, bool, char, double>",			L"SizeOfTypes<int, bool, char, double>",		L"unsigned __int32"											);
@@ -189,6 +193,8 @@ using Func = R(*)(Ptr<TArgs>&&...);
 )";
 	COMPILE_PROGRAM(program, pa, input);
 	
+	AssertType(pa, L"Func",											L"Func",										L"<::Func::[R], ::Func::[...TArgs]> any_t"					);
+
 	AssertType(pa, L"Func<bool>",									L"Func<bool>",									L"bool () *"												);
 	AssertType(pa, L"Func<bool, int>",								L"Func<bool, int>",								L"bool (int * &&) *"										);
 	AssertType(pa, L"Func<bool, int, bool, char, double>",			L"Func<bool, int, bool, char, double>",			L"bool (int * &&, bool * &&, char * &&, double * &&) *"		);
@@ -201,7 +207,7 @@ template<typename T>
 using Ptr = T*;
 
 template<typename R, typename... TArgs>
-using Init = R(*)(decltype({Ptr<TArgs>&&...}));
+using Init = R(*)(decltype({Ptr<TArgs>*{}...}));
 
 char F(char, bool);
 bool F(bool, char);
@@ -214,18 +220,106 @@ template<typename... TArgs>
 using FOf = decltype(F(TArgs()...););
 )";
 	COMPILE_PROGRAM(program, pa, input);
+
+#define TYPE_LIST_STRING(A, B, C, D, E, F)		\
+	L"<::FOf::[...TArgs]> " L ## #A				\
+	L"<::FOf::[...TArgs]> " L ## #B				\
+	L"<::FOf::[...TArgs]> " L ## #C				\
+	L"<::FOf::[...TArgs]> " L ## #D				\
+	L"<::FOf::[...TArgs]> " L ## #E				\
+	L"<::FOf::[...TArgs]> " L ## #F				\
+
 	
-	AssertType(pa, L"Init<bool>",									L"Init<bool>",									L"bool ({}) *"												);
-	AssertType(pa, L"Init<bool, int>",								L"Init<bool, int>",								L"bool ({int * &&}) *"										);
-	AssertType(pa, L"Init<bool, int, bool, char, double>",			L"Init<bool, int, bool, char, double>",			L"bool ({int * &&, bool * &&, char * &&, double * &&}) *"	);
-	AssertType(pa, L"FOf<>",										L"FOf<>",										L"double"													);
-	AssertType(pa, L"FOf<int>",										L"FOf<int>",									L"void"														);
-	AssertType(pa, L"FOf<char, bool>",								L"FOf<char, bool>",								L"char"														);
-	AssertType(pa, L"FOf<bool, char>",								L"FOf<bool, char>",								L"bool"														);
-	AssertType(pa, L"FOf<int, float, double>",						L"FOf<int, float, double>",						L"int"														);
-	AssertType(pa, L"FOf<int*, float*, double*>",					L"FOf<int *, float *, double *>",				L"int *"													);
+	AssertType(pa, L"Init",											L"Init",										L"<::Init::[R], ::Init::[...TArgs]> any_t"									);
+	AssertType(pa, L"FOf",											L"FOf",											TYPE_LIST_STRING(char, bool, int, int *, double, void)						);
+
+	AssertType(pa, L"Init<bool>",									L"Init<bool>",									L"bool ({}) *"																);
+	AssertType(pa, L"Init<bool, int>",								L"Init<bool, int>",								L"bool ({int * * $PR}) *"													);
+	AssertType(pa, L"Init<bool, int, bool, char, double>",			L"Init<bool, int, bool, char, double>",			L"bool ({int * * $PR, bool * * $PR, char * * $PR, double * * $PR}) *"		);
+	AssertType(pa, L"FOf<>",										L"FOf<>",										L"double"																	);
+	AssertType(pa, L"FOf<int>",										L"FOf<int>",									L"void"																		);
+	AssertType(pa, L"FOf<char, bool>",								L"FOf<char, bool>",								L"char"																		);
+	AssertType(pa, L"FOf<bool, char>",								L"FOf<bool, char>",								L"bool"																		);
+	AssertType(pa, L"FOf<int, float, double>",						L"FOf<int, float, double>",						L"int"																		);
+	AssertType(pa, L"FOf<int*, float*, double*>",					L"FOf<int *, float *, double *>",				L"int *"																	);
+
+#undef TYPE_LIST_STRING
 }
 
 TEST_CASE(TestParseTypeAlias_VTA_ApplyOn_VTA_Default)
 {
+	auto input = LR"(
+template<typename T = int*>											using One =				decltype({T{}});
+template<typename T = int*, typename U = char*>						using Two =				decltype({T{}, U{}});
+template<typename... TArgs>											using Vta =				decltype({TArgs{}...});
+template<typename T = int*, typename... TArgs>						using OneVta =			decltype({T{}, TArgs{}...});
+template<typename T = int*, typename U = char*, typename... TArgs>	using TwoVta =			decltype({T{}, U{}, TArgs{}...});
+
+template<typename... Ts>											using ApplyOne =		One<Ts...>;
+template<typename... Ts>											using ApplyTwo =		Two<Ts...>;
+template<typename... Ts>											using ApplyVta =		Vta<Ts...>;
+template<typename... Ts>											using ApplyOneVta =		OneVta<Ts...>;
+template<typename... Ts>											using ApplyTwoVta =		TwoVta<Ts...>;
+
+template<typename T, typename... Ts>								using ApplyOne_1 =		One<T, Ts...>;
+template<typename T, typename... Ts>								using ApplyTwo_1 =		Two<T, Ts...>;
+template<typename T, typename... Ts>								using ApplyVta_1 =		Vta<T, Ts...>;
+template<typename T, typename... Ts>								using ApplyOneVta_1 =	OneVta<T, Ts...>;
+template<typename T, typename... Ts>								using ApplyTwoVta_1 =	TwoVta<T, Ts...>;
+)";
+	COMPILE_PROGRAM(program, pa, input);
+	
+	AssertType(pa, L"ApplyOne",									L"ApplyOne",									L"<::ApplyOne::[...Ts]> {any_t $PR}"											);
+	AssertType(pa, L"ApplyTwo",									L"ApplyTwo",									L"<::ApplyTwo::[...Ts]> {any_t $PR, any_t $PR}"									);
+	AssertType(pa, L"ApplyVta",									L"ApplyVta",									L"<::ApplyVta::[...Ts]> any_t"													);
+	AssertType(pa, L"ApplyOneVta",								L"ApplyOneVta",									L"<::ApplyOneVta::[...Ts]> any_t"												);
+	AssertType(pa, L"ApplyTwoVta",								L"ApplyTwoVta",									L"<::ApplyTwoVta::[...Ts]> any_t"												);
+	
+	AssertType(pa, L"ApplyOne_1",								L"ApplyOne_1",									L"<::ApplyOne_1::[T], ::ApplyOne_1::[...Ts]> {::ApplyOne_1::[T] $PR}"			);
+	AssertType(pa, L"ApplyTwo_1",								L"ApplyTwo_1",									L"<::ApplyTwo_1::[T], ::ApplyTwo_1::[...Ts]> {::ApplyTwo_1::[T] $PR, any_t $PR}");
+	AssertType(pa, L"ApplyVta_1",								L"ApplyVta_1",									L"<::ApplyVta_1::[T], ::ApplyVta_1::[...Ts]> any_t"								);
+	AssertType(pa, L"ApplyOneVta_1",							L"ApplyOneVta_1",								L"<::ApplyOneVta_1::[T], ::ApplyOneVta_1::[...Ts]> any_t"						);
+	AssertType(pa, L"ApplyTwoVta_1",							L"ApplyTwoVta_1",								L"<::ApplyTwoVta_1::[T], ::ApplyTwoVta_1::[...Ts]> any_t"						);
+	
+	AssertType(pa, L"ApplyOne<>",								L"ApplyOne<>",									L"{int * $PR}"																	);
+	AssertType(pa, L"ApplyOne<int>",							L"ApplyOne<int>",								L"{int $PR}"																	);
+	AssertType(pa, L"ApplyOne<int, char>",						L"ApplyOne<int, char>"																											);
+	AssertType(pa, L"ApplyOne<int, char, bool>",				L"ApplyOne<int, char, bool>"																									);
+	AssertType(pa, L"ApplyTwo<>",								L"ApplyTwo<>",									L"{int * $PR, char * $PR}"														);
+	AssertType(pa, L"ApplyTwo<int>",							L"ApplyTwo<int>",								L"{int $PR, char * $PR}"														);
+	AssertType(pa, L"ApplyTwo<int, char>",						L"ApplyTwo<int, char>"							L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyTwo<int, char, bool>",				L"ApplyTwo<int, char, bool>"																									);
+	AssertType(pa, L"ApplyVta<>",								L"ApplyVta<>",									L"{}"																			);
+	AssertType(pa, L"ApplyVta<int>",							L"ApplyVta<int>",								L"{int $PR}"																	);
+	AssertType(pa, L"ApplyVta<int, char>",						L"ApplyVta<int, char>",							L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyVta<int, char, bool>",				L"ApplyVta<int, char, bool>",					L"{int $PR, char $PR, bool $PR}"												);
+	AssertType(pa, L"ApplyOneVta<>",							L"ApplyOneVta<>",								L"{}"																			);
+	AssertType(pa, L"ApplyOneVta<int>",							L"ApplyOneVta<int>",							L"{int $PR}"																	);
+	AssertType(pa, L"ApplyOneVta<int, char>",					L"ApplyOneVta<int, char>",						L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyOneVta<int, char, bool>",				L"ApplyOneVta<int, char, bool>",				L"{int $PR, char $PR, bool $PR}"												);
+	AssertType(pa, L"ApplyTwoVta<>",							L"ApplyTwoVta<>",								L"{}"																			);
+	AssertType(pa, L"ApplyTwoVta<int>",							L"ApplyTwoVta<int>",							L"{int $PR}"																	);
+	AssertType(pa, L"ApplyTwoVta<int, char>",					L"ApplyTwoVta<int, char>",						L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyTwoVta<int, char, bool>",				L"ApplyTwoVta<int, char, bool>",				L"{int $PR, char $PR, bool $PR}"												);
+	
+	AssertType(pa, L"ApplyOne_1<>",								L"ApplyOne_1<>"																													);
+	AssertType(pa, L"ApplyOne_1<int>",							L"ApplyOne_1<int>",								L"{int $PR}"																	);
+	AssertType(pa, L"ApplyOne_1<int, char>",					L"ApplyOne_1<int, char>"																										);
+	AssertType(pa, L"ApplyOne_1<int, char, bool>",				L"ApplyOne_1<int, char, bool>"																									);
+	AssertType(pa, L"ApplyTwo_1<>",								L"ApplyTwo_1<>"																													);
+	AssertType(pa, L"ApplyTwo_1<int>",							L"ApplyTwo_1<int>"								L"{int $PR, char * $PR}"														);
+	AssertType(pa, L"ApplyTwo_1<int, char>",					L"ApplyTwo_1<int, char>"						L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyTwo_1<int, char, bool>",				L"ApplyTwo_1<int, char, bool>"																									);
+	AssertType(pa, L"ApplyVta_1<>",								L"ApplyVta_1<>"																													);
+	AssertType(pa, L"ApplyVta_1<int>",							L"ApplyVta_1<int>",								L"{int $PR}"																	);
+	AssertType(pa, L"ApplyVta_1<int, char>",					L"ApplyVta_1<int, char>",						L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyVta_1<int, char, bool>",				L"ApplyVta_1<int, char, bool>",					L"{int $PR, char $PR, bool $PR}"												);
+	AssertType(pa, L"ApplyOneVta_1<>",							L"ApplyOneVta_1<>"																												);
+	AssertType(pa, L"ApplyOneVta_1<int>",						L"ApplyOneVta_1<int>",							L"{int $PR}"																	);
+	AssertType(pa, L"ApplyOneVta_1<int, char>",					L"ApplyOneVta_1<int, char>",					L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyOneVta_1<int, char, bool>",			L"ApplyOneVta_1<int, char, bool>",				L"{int $PR, char $PR, bool $PR}"												);
+	AssertType(pa, L"ApplyTwoVta_1<>",							L"ApplyTwoVta_1<>"																												);
+	AssertType(pa, L"ApplyTwoVta_1<int>",						L"ApplyTwoVta_1<int>",							L"{int $PR}"																	);
+	AssertType(pa, L"ApplyTwoVta_1<int, char>",					L"ApplyTwoVta_1<int, char>",					L"{int $PR, char $PR}"															);
+	AssertType(pa, L"ApplyTwoVta_1<int, char, bool>",			L"ApplyTwoVta_1<int, char, bool>",				L"{int $PR, char $PR, bool $PR}"												);
 }
