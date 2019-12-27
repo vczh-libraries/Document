48e4922759184324e96e612aaa737401a392d432
Zihan Chen <vczh@163.com>
2019-12-27 15:35:29 -0800

    Update TestOverloadingGenericFunction.cpp

--- Tools/CppDoc/UnitTest/TestOverloadingGenericFunction.cpp
+++ Tools/CppDoc/UnitTest/TestOverloadingGenericFunction.cpp
@@ -1,6 +1,6 @@
 #include "Util.h"
 
-namespace Input__TestOverloadingGenericFunction_TypeInfer
+namespace Input__TestOverloadingGenericFunction_TypeInferSimple
 {
 	TEST_DECL(
 		const int ci = 0;
@@ -13,6 +13,15 @@ namespace Input__TestOverloadingGenericFunction_TypeInfer
 		template<typename T>
 		auto Simple2(T a, T b = {}) { return a; }
 		void Simple2(...) {}
+	);
+}
+
+namespace Input__TestOverloadingGenericFunction_TypeInferVariant
+{
+	TEST_DECL(
+		const int ci = 0;
+		volatile int vi = 0;
+		const volatile int cvi = 0;
 
 		template<typename... Ts>
 		struct Types {};
@@ -22,11 +31,35 @@ namespace Input__TestOverloadingGenericFunction_TypeInfer
 	);
 }
 
+namespace Input__TestOverloadingGenericFunction_TypeInferKinds
+{
+	TEST_DECL(
+		template<typename... Ts>
+		struct Types {};
+
+		template<typename T>
+		T Value();
+
+		template<typename T>							auto LRef(T&)								-> T					;
+		template<typename T>							auto RRef(T&&)								-> T					;
+		template<typename T>							auto Pointer(T*)							-> T					;
+		template<typename R, typename... TArgs>			auto Function(R(*)(TArgs...))				-> Types<R, TArgs...>	;
+		template<typename T, typename U>				auto Member(T U::*)							-> Types<T, U>			;
+		template<typename T>							auto C(const T)								-> T					;
+		template<typename T>							auto V(volatile T)							-> T					;
+		template<typename T>							auto CV(const volatile T)					-> T					;
+		template<typename... TArgs>						auto VtaPtr(TArgs*...)						-> Types<TArgs...>		;
+		template<typename... TArgs>						auto VtaTypes(Types<TArgs...>)				-> Types<TArgs...>		;
+		template<typename R, typename... TArgs>			auto VtaFunc(Types<R(*)(TArgs*)...>)		-> Types<TArgs...>		;
+		template<typename... TRs, typename... TArgs>	auto VtaFunc2(Types<TRs(*)(TArgs*)...>)		-> Types<TArgs...>		;
+	);
+}
+
 TEST_FILE
 {
 	TEST_CATEGORY(L"Template argument deduction (simple)")
 	{
-		using namespace Input__TestOverloadingGenericFunction_TypeInfer;
+		using namespace Input__TestOverloadingGenericFunction_TypeInferSimple;
 		COMPILE_PROGRAM(program, pa, input);
 
 		ASSERT_OVERLOADING_SIMPLE(Simple(1),					__int32);
@@ -57,9 +90,15 @@ TEST_FILE
 
 	TEST_CATEGORY(L"Template argument deduction (variant)")
 	{
-		using namespace Input__TestOverloadingGenericFunction_TypeInfer;
+		using namespace Input__TestOverloadingGenericFunction_TypeInferVariant;
 		COMPILE_PROGRAM(program, pa, input);
 
 		ASSERT_OVERLOADING_SIMPLE((Variant(1, 1.0, 1.f, ci, vi, cvi)),			Types<__int32, double, float, __int32, __int32, __int32>);
+	});
+
+	TEST_CATEGORY(L"Template argument deduction (kinds)")
+	{
+		using namespace Input__TestOverloadingGenericFunction_TypeInferKinds;
+		COMPILE_PROGRAM(program, pa, input);
 	});
 }
