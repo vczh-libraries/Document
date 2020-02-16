fd99a17eb2405314403518ec2c5a9e7bdb3e8e6a
Zihan Chen <vczh@163.com>
2020-02-16 00:43:22 -0800

    Update TestOverloadingGenericMethodInfer.cpp

--- Tools/CppDoc/UnitTest/TestOverloadingGenericMethodInfer.cpp
+++ Tools/CppDoc/UnitTest/TestOverloadingGenericMethodInfer.cpp
@@ -7,8 +7,18 @@ namespace Input__TestOverloadingGenericFunction_Method
 		template<typename... Ts>
 		struct MakeTuple
 		{
+			MakeTuple<int, Ts...> operator++();
+
+			MakeTuple<Ts..., int> operator++(int);
+
 			template<typename T>
 			MakeTuple<Ts..., T> operator<<(T);
+
+			template<typename T>
+			MakeTuple<Ts..., T> operator[](T);
+
+			template<typename... Us>
+			MakeTuple<Ts..., Us...> operator+(MakeTuple<Us...>);
 		};
 	);
 }
@@ -19,6 +29,34 @@ TEST_FILE
 	{
 		using namespace Input__TestOverloadingGenericFunction_Method;
 		COMPILE_PROGRAM(program, pa, input);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(++MakeTuple<>()),
+			L"((++ MakeTuple<>()))",
+			L"::MakeTuple<{__int32 $PR}> $PR",
+			MakeTuple<int>
+		);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(++++++MakeTuple<>()),
+			L"((++ (++ (++ MakeTuple<>()))))",
+			L"::MakeTuple<{__int32 $PR, __int32 $PR, __int32 $PR}> $PR",
+			MakeTuple<int, int, int>
+		);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(MakeTuple<>()++),
+			L"((MakeTuple<>() ++))",
+			L"::MakeTuple<{__int32 $PR}> $PR",
+			MakeTuple<int>
+		);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(MakeTuple<>()++++++),
+			L"((((MakeTuple<>() ++) ++) ++))",
+			L"::MakeTuple<{__int32 $PR, __int32 $PR, __int32 $PR}> $PR",
+			MakeTuple<int, int, int>
+		);
 
 		ASSERT_OVERLOADING_VERBOSE(
 			(MakeTuple<>() << 1),
@@ -33,9 +71,30 @@ TEST_FILE
 			L"::MakeTuple<{__int32 $PR, double $PR, float $PR, char $PR, wchar_t $PR, bool $PR}> $PR",
 			MakeTuple<int, double, float, char, wchar_t, bool>
 		);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(MakeTuple<>()[0]),
+			L"(MakeTuple<>()[0])",
+			L"::MakeTuple<{__int32 $PR}> $PR",
+			MakeTuple<int>
+		);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(MakeTuple<>()[0][1][2]),
+			L"(MakeTuple<>()[0][1][2])",
+			L"::MakeTuple<{__int32 $PR, __int32 $PR, __int32 $PR}> $PR",
+			MakeTuple<int, int, int>
+		);
+
+		ASSERT_OVERLOADING_VERBOSE(
+			(MakeTuple<>()['a'][true][1.0] + ++++++MakeTuple<>()),
+			L"((MakeTuple<>()['a'][true][1.0] + (++ (++ (++ MakeTuple<>())))))",
+			L"::MakeTuple<{char $PR, bool $PR, double $PR, __int32 $PR, __int32 $PR, __int32 $PR}> $PR",
+			MakeTuple<char, bool, double, int, int, int>
+		);
 	});
 
-	// test methods/operators
+	// test methods
 	// use multiple levels of type arguments in function arguments
 	// call methods/operators on "this"
 }
