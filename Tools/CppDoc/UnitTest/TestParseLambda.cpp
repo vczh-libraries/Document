b3dd31f7488d706eb5aeabcf68b3947b036233c1
Zihan Chen <vczh@163.com>
2020-05-30 02:20:28 -0700

    New test case

--- Tools/CppDoc/UnitTest/TestParseLambda.cpp
+++ Tools/CppDoc/UnitTest/TestParseLambda.cpp
@@ -241,6 +241,234 @@ struct S
 
 	TEST_CATEGORY(L"Capturing int in nested lambda")
 	{
-		// TODO:
+		auto input = LR"(
+int a1 = 0;
+struct S
+{
+	int a2 = 0;
+	void F()
+	{
+		int b = 0;
+		volatile int& c = 0;
+		const volatile int&& d = 0;
+
+		auto l1 = [=](int e)
+		{
+			auto m1 = [=](int f)		{};
+			auto m2 = [=](int f)mutable	{};
+			auto m3 = [&](int f)		{};
+		};
+
+		auto l2 = [=](int e)mutable
+		{
+			auto m1 = [=](int f)		{};
+			auto m2 = [=](int f)mutable	{};
+			auto m3 = [&](int f)		{};
+		};
+
+		auto l3 = [&](int e)
+		{
+			auto m1 = [=](int f)		{};
+			auto m2 = [=](int f)mutable	{};
+			auto m3 = [&](int f)		{};
+		};
+	}
+};
+)";
+		COMPILE_PROGRAM(program, ppa, input);
+
+		auto stat = ppa.root
+			->TryGetChildren_NFb(L"S")->Get(0).childSymbol
+			->TryGetChildren_NFb(L"F")->Get(0).childSymbol->GetImplSymbols_F()[0]
+			->TryGetChildren_NFb(L"$")->Get(0).childSymbol
+			->GetStat_N().Cast<BlockStat>();
+
+		int b = 0;
+		volatile int& c = (volatile int&)b;
+		const volatile int&& d = (const volatile int&&)b;
+
+		{
+			auto statl = getInsideLambda(stat, L"l1");
+			auto testl = [=, &ppa](int e)
+			{
+				{
+					auto statm = getInsideLambda(statl, L"m1");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [=, &pa](int f)
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m1")
+					{
+						testm(0);
+					});
+				}
+				{
+					auto statm = getInsideLambda(statl, L"m2");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [=, &pa](int f)mutable
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m2")
+					{
+						testm(0);
+					});
+				}
+				{
+					auto statm = getInsideLambda(statl, L"m3");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [&](int f)
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m3")
+					{
+						testm(0);
+					});
+				}
+			};
+			TEST_CATEGORY(L"l1")
+			{
+				testl(0);
+			});
+		}
+		{
+			auto statl = getInsideLambda(stat, L"l2");
+			auto testl = [=, &ppa](int e)mutable
+			{
+				{
+					auto statm = getInsideLambda(statl, L"m1");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [=, &pa](int f)
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m1")
+					{
+						testm(0);
+					});
+				}
+				{
+					auto statm = getInsideLambda(statl, L"m2");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [=, &pa](int f)mutable
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m2")
+					{
+						testm(0);
+					});
+				}
+				{
+					auto statm = getInsideLambda(statl, L"m3");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [&](int f)
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m3")
+					{
+						testm(0);
+					});
+				}
+			};
+			TEST_CATEGORY(L"l2")
+			{
+				testl(0);
+			});
+		}
+		{
+			auto statl = getInsideLambda(stat, L"l3");
+			auto testl = [&](int e)
+			{
+				{
+					auto statm = getInsideLambda(statl, L"m1");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [=, &pa](int f)
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 const &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m1")
+					{
+						testm(0);
+					});
+				}
+				{
+					auto statm = getInsideLambda(statl, L"m2");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [=, &pa](int f)mutable
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m2")
+					{
+						testm(0);
+					});
+				}
+				{
+					auto statm = getInsideLambda(statl, L"m3");
+					auto pa = ppa.AdjustForDecl(statm->symbol);
+					auto testm = [&](int f)
+					{
+						(void)(b, c, d, e, f);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((b), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((c), __int32 volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((d), __int32 const volatile &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((e), __int32 &);
+						ASSERT_OVERLOADING_SIMPLE_LVALUE((f), __int32 &);
+					};
+					TEST_CATEGORY(L"m3")
+					{
+						testm(0);
+					});
+				}
+			};
+			TEST_CATEGORY(L"l3")
+			{
+				testl(0);
+			});
+		}
 	});
 }
