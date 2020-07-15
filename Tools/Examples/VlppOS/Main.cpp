874c85d30105f8ac1a35040f7131bf4b938159c9
Zihan Chen <vczh@163.com>
2020-07-15 14:20:51 -0700

    ...

---
+++ Tools/Examples/VlppOS/Main.cpp
@@ -0,0 +1,10 @@
+#include <VlppOS.h>
+
+using namespace vl;
+using namespace vl::collections;
+using namespace vl::unittest;
+using namespace vl::console;
+using namespace vl::stream;
+using namespace vl::filesystem;
+
+#include "Example.h"
