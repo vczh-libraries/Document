0f95bbef7d68671228026b359e51d496400055bf
vczh <vczh@outlook.com>
2023-04-26 23:27:40 -0700

    ...

--- Tools/CppDoc/UnitTest/TestOverloadingOperator_Input4.h
+++
@@ -1,7 +0,0 @@
-extern test_overloading::X x;
-extern test_overloading::Y y;
-extern test_overloading::Z z;
-
-extern const test_overloading::X cx;
-extern const test_overloading::Y cy;
-extern const test_overloading::Z cz;
